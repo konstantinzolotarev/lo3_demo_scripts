230ea5dbf1fccddf69496fd84f3fb9adb5fada70
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2017-01-16 23:05:16 +0200

    VLO3-61 listener added

--- node1.js
+++ node1.js
@@ -2,6 +2,7 @@
 
 const erisDb = require('eris-db-promise')
 const config = require('config')
+const _ = require('lodash')
 
 if (!process.argv[2]) {
   console.log('==========================')
@@ -14,13 +15,43 @@ const deployedAddress = process.argv[2]
 const eris = erisDb.createInstance(config.rpc.node1)
 
 let subId
+let interval
 
-// eris
-//   .events
-//   .eventSubscribe()
+function setupListener () {
+  if (interval)
+    return
+  interval = setInterval(() => {
+    eris
+      .events
+      .eventPoll(subId)
+      .then((events) => {
+        if (_.isArray(events) && events.length > 0) {
+          console.log('==========================')
+          console.log(events)
+          console.log('==========================')
+        }
+      })
+  }, 1000)
+}
+
+eris
+  .events
+  .eventSubscribe(`Acc/${deployedAddress}/Call`)
+  .then((newSubId) => {
+    if (!_.isString(newSubId) || !newSubId.length)
+      return Promise.reject(new Error('No subscription id received !'))
+    subId = newSubId
+    console.log('==========================')
+    console.log(subId)
+    console.log('==========================')
+  })
+  .then(() => setupListener())
 
 // Handle kill event
 process.on('SIGINT', () => {
+  if (interval)
+    clearInterval(interval)
+
   if (!subId)
     return process.exit()
 
